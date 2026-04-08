4ab693e2ffdb071e87c4ee8682448e84d0f4ed34
Avikesh Gurjar <230293419+avikeshgurjar@users.noreply.github.com>
2026-04-08 20:11:18 +0530

    Delete node_modules directory

--- node_modules/react-open-weather/tests/test-utils.js
+++
@@ -1,6 +0,0 @@
-import TestRenderer from 'react-test-renderer';
-
-export const checkSnapshot = (component, options) => {
-  const tree = TestRenderer.create(component, options).toJSON();
-  return expect(tree).toMatchSnapshot();
-};
